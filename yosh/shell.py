206566d458c677ebdc812355d9b079fd1db191d2
Supasate Choochaisri <supasate.c@gmail.com>
2016-07-06 04:50:52 +0700

    Use fork to prevent shell process memory replacement

--- yosh/shell.py
+++ yosh/shell.py
@@ -9,8 +9,24 @@ def tokenize(string):
     return shlex.split(string)
 
 def execute(cmd_tokens):
-    # Execute command
-    os.execvp(cmd_tokens[0], cmd_tokens)
+    # Fork a child shell process
+    # If the current process is a child process, pid = 0
+    # If the current process is a parent process, pid = process id of its child process
+    pid = os.fork()
+
+    if pid == 0:
+        # Child process
+        # Replace the child shell process with the program called with exec
+        os.execvp(cmd_tokens[0], cmd_tokens)
+    elif pid > 0:
+        # Parent process
+        while True:
+            # Wait response status from its child process (identified with pid)
+            wpid, status = os.waitpid(pid, 0)
+
+            # Finish waiting if its child process exits normally or is terminated by a signal
+            if os.WIFEXITED(status) or os.WIFSIGNALED(status):
+                break
 
     # Return status indicating to wait for next command in shell_loop
     return SHELL_STATUS_RUN
