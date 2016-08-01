02348f94a25d5db7c6465465203c30dd994d030c
Supasate Choochaisri <supasate.c@gmail.com>
2016-08-01 18:49:19 +0700

    Fix author info

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import setup
 setup(name='yosh',
       version='1.0',
       description='Your own shell',
-      author='Supasate',
-      author_email='tanyuliang2@gmail.com',
+      author='Supasate Choochaisri',
+      author_email='supasate.c@gmail.com',
       url='https://github.com/supasate/yosh',
       packages=['yosh', 'yosh.builtins'],
       entry_points="""
